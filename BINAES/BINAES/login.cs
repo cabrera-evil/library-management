648f8fb56032a3905544fe75355b0c42829bbc9c
cabrera-evil <douglascabrera.dev@gmail.com>
2022-06-12 23:36:32 -0600

    Add: Login

---
+++ BINAES/BINAES/login.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace BINAES
+{
+    public partial class frm_login : Form
+    {
+        public frm_login()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void txt_user_Enter(object sender, EventArgs e)
+        {
+            if (txt_user.Text == "User")
+            {
+                txt_user.Text = "";
+                txt_user.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txt_user_Leave(object sender, EventArgs e)
+        {
+            if (txt_user.Text == "")
+            {
+                txt_user.Text = "User";
+                txt_user.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txt_password_Enter(object sender, EventArgs e)
+        {
+            if (txt_password.Text == "Password")
+            {
+                txt_password.Text = "";
+                txt_password.ForeColor = Color.LightGray;
+                txt_password.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txt_password_Leave(object sender, EventArgs e)
+        {
+
+            if (txt_password.Text == "")
+            {
+                txt_password.Text = "Password";
+                txt_password.ForeColor = Color.LightGray;
+                txt_password.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btn_minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frm_login_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0); 
+        }
+
+        private void pct_logo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
