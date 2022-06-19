a7aff8b88e6fb5451f4a783c166de4a5deb661bc
cabrera-evil <douglascabrera.dev@gmail.com>
2022-06-19 13:45:44 -0600

    Add: SQL Script

---
+++ BINAES/BINAES/SQL Server/BINAES_POPULATE.sql
@@ -0,0 +1,69 @@
+USE BINAES;
+GO
+--OCCUPANCY
+INSERT INTO OCCUPANCY VALUES
+('Student'),
+('Worker')
+GO
+--ROLE
+INSERT INTO ROLE_ VALUES
+('Admin'),
+('Client');
+GO
+--INSTITUTION
+insert into INSTITUTION (institution_name) values ('University of Advancing Technology (UAT)');
+insert into INSTITUTION (institution_name) values ('Warner Pacific College');
+insert into INSTITUTION (institution_name) values ('Maejo University');
+insert into INSTITUTION (institution_name) values ('School of Design and Crafts');
+insert into INSTITUTION (institution_name) values ('University of Iasi');
+insert into INSTITUTION (institution_name) values ('University of Stirling');
+insert into INSTITUTION (institution_name) values ('Kocaeli University');
+insert into INSTITUTION (institution_name) values ('Fachhochschule Dortmund');
+insert into INSTITUTION (institution_name) values ('University of Nevada - Reno');
+insert into INSTITUTION (institution_name) values ('Fachhochschule Kaiserslautern');
+insert into INSTITUTION (institution_name) values ('Université Paris Nord (Paris XIII)');
+insert into INSTITUTION (institution_name) values ('Shenkar School of Engineering & Design');
+insert into INSTITUTION (institution_name) values ('Samara State Academy for Railway Transportation');
+insert into INSTITUTION (institution_name) values ('Villa Julie College');
+insert into INSTITUTION (institution_name) values ('University of Latvia');
+insert into INSTITUTION (institution_name) values ('Universidad de Ciencias Aplicadas y Ambientales (UDCA)');
+insert into INSTITUTION (institution_name) values ('Chongqing Normal University Foreign Trade and Business College');
+insert into INSTITUTION (institution_name) values ('1 December University of Alba Iulia');
+insert into INSTITUTION (institution_name) values ('Nebraska Methodist College of Nursing and Allied Health');
+insert into INSTITUTION (institution_name) values ('Universität Osnabrück');
+insert into INSTITUTION (institution_name) values ('Western New Mexico University');
+insert into INSTITUTION (institution_name) values ('Zhongnan University of Economics and Law');
+insert into INSTITUTION (institution_name) values ('Universidade Autónoma de Lisboa Luís de Camoes');
+insert into INSTITUTION (institution_name) values ('Niger Delta University');
+insert into INSTITUTION (institution_name) values ('Tianjin University');
+insert into INSTITUTION (institution_name) values ('Universidad Capitain General Gerardo Barrios');
+insert into INSTITUTION (institution_name) values ('Universidad de Zaragoza');
+insert into INSTITUTION (institution_name) values ('Majmaah University');
+insert into INSTITUTION (institution_name) values ('Politeknik Negeri Jakarta');
+insert into INSTITUTION (institution_name) values ('Malaysia University of Science and Technology (MUST)');
+insert into INSTITUTION (institution_name) values ('Troy University, Montgomery');
+insert into INSTITUTION (institution_name) values ('International University of Travnik');
+insert into INSTITUTION (institution_name) values ('Ajou University');
+insert into INSTITUTION (institution_name) values ('Universidade Vale do Rio dos Sinos');
+insert into INSTITUTION (institution_name) values ('University of Arkansas at Monticello');
+insert into INSTITUTION (institution_name) values ('Minamikyushu University');
+insert into INSTITUTION (institution_name) values ('Zhetysu State University ');
+insert into INSTITUTION (institution_name) values ('Kurume Institute of Technology');
+insert into INSTITUTION (institution_name) values ('Ukrainian National Forestry University');
+insert into INSTITUTION (institution_name) values ('Luther College');
+insert into INSTITUTION (institution_name) values ('Westminster College of Salt Lake City');
+insert into INSTITUTION (institution_name) values ('Geneva College');
+insert into INSTITUTION (institution_name) values ('Hiiraan University');
+insert into INSTITUTION (institution_name) values ('University of Virginia');
+insert into INSTITUTION (institution_name) values ('Westsächsische Hochschule Zwickau (FH)');
+insert into INSTITUTION (institution_name) values ('Yelets State University');
+insert into INSTITUTION (institution_name) values ('Universidad Latina de Panamá');
+insert into INSTITUTION (institution_name) values ('Mehrabyan Medical Institute and Medical College');
+insert into INSTITUTION (institution_name) values ('University of Agriculture Faisalabad');
+GO
+--USER  
+GO
+INSERT INTO USER_ VALUES
+('Douglas Cabrera', 'Avenida Jerusalen, San Salvador', '+503 71668439', 'https://yt3.ggpht.com/hrURZ6XY_SFhFUcTyQA6EGQ1EK9HJaCibZxJQayQc7EaXtkI93kn1JQ_lvf7md67NDZ60V49Jw=s88-c-k-c0x00ffffff-no-rj', '00133121@uca.edu.sv', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 1, 1, 1),
+('Monica Cabañas', 'idk', '+503 79177020', 'https://yt3.ggpht.com/hrURZ6XY_SFhFUcTyQA6EGQ1EK9HJaCibZxJQayQc7EaXtkI93kn1JQ_lvf7md67NDZ60V49Jw=s88-c-k-c0x00ffffff-no-rj', '00079921@uca.edu.sv', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 1, 1, 1);
+USE master;
